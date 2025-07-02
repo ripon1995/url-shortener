2c91ea71564ffe1af8afe76db9574d86c5808d83
Ripon <alamin.ripon@brainstation-23.com>
2025-07-02 15:59:20 +0600

    What?  - database setup

--- urlshortener_project/settings.py
+++ urlshortener_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "urlshortener_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "urlshortener_db",
+        "USER": "postgres",
+        "PASSWORD": "rootless",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
