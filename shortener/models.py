94c663fe9f220d6cce50cb3f9b98386f64d5eafe
Ripon <alamin.ripon@brainstation-23.com>
2025-07-02 14:52:17 +0600

    What?  - shortener model created

--- shortener/models.py
+++ shortener/models.py
@@ -8,6 +8,7 @@ class ShortenedURL(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
+    is_active = models.BooleanField(default=True, verbose_name="Is Active")
 
     class Meta:
         verbose_name = "Shortened URL"
