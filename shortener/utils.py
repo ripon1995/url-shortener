8a879e1ec98b2781a32611dd1f4c642d0bd7bbe7
Ripon <alamin.ripon@brainstation-23.com>
2025-07-02 17:36:07 +0600

    What?  - unused print statement removed

--- shortener/utils.py
+++ shortener/utils.py
@@ -5,7 +5,6 @@ def shorten_url(url):
     shortener = pyshorteners.Shortener()
     try:
         shortened_url = shortener.tinyurl.short(url)
-        print("shortened url : ", shortened_url)
         return shortened_url
     except Exception as e:
         return str(e)
