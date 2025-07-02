920ba9686f448e4b422623bde61fe397ee832cf7
Ripon <alamin.ripon@brainstation-23.com>
2025-07-02 18:56:49 +0600

    What?  - detail api added

--- shortener/views.py
+++ shortener/views.py
@@ -22,3 +22,38 @@ class ShortenUrlListCreateAPIView(APIView):
             shortened_url=shorten_url(serializer.validated_data["original_url"])
         )
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ShortenUrlRetrieveUpdateDestroyAPIView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return ShortenedURL.objects.get(pk=pk)
+        except ShortenedURL.DoesNotExist:
+            return None
+
+    def get(self, request, pk, *args, **kwargs):
+        shortened_url = self.get_object(pk)
+        if not shortened_url:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ShortenedURLListCreateSerializer(shortened_url)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, *args, **kwargs):
+        shortened_url = self.get_object(pk)
+        if not shortened_url:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ShortenedURLListCreateSerializer(shortened_url, data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+        serializer.save(
+            shortened_url=shorten_url(serializer.validated_data["original_url"])
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk, *args, **kwargs):
+        shortened_url = self.get_object(pk)
+        if not shortened_url:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        shortened_url.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
